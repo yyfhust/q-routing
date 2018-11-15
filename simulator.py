0aa3d47bef127467b23f17e6c26752aec7658450
Kevin Chang <kevin@kchang.me>
2018-11-14 19:53:23 -0800

    Made graph connected lol.

--- simulator.py
+++ simulator.py
@@ -20,16 +20,17 @@ MAX_LATENCY = 300
 LATENCY_STD = 5
 
 class Node:
-  def __init__(self):
+  def __init__(self, id):
     self.edges = []
+    self.id = id
 
   def addEdge(self, edge):
     self.edges.append(edge)
 
 class Edge:
-  def __init__(self, n1, n2):
-    self.n1 = n1
-    self.n2 = n2
+  def __init__(self, ni_1, ni_2):
+    self.ni_1 = ni_1
+    self.ni_2 = ni_2
 
     # Generate a random latency and drop rate within a range,
     # drawn from a uniform distribution.
@@ -45,13 +46,16 @@ class Edge:
     return rnd.randint(0, 100) <= self.dropRate * 100
 
   def __eq__(self, other): 
-    return self.n1 == other.n1 and self.n2 == other.n1
+    return (self.ni_1 == other.ni_2 and self.ni_2 == other.ni_1) or (self.ni_1 == other.ni_1 and self.ni_2 == other.ni_2)
+
+  def __hash__(self):
+    return hash(tuple((self.ni_1, self.ni_2)))
 
 class Packet:
   # Initialize path to include the source / starting node.
-  def __init__(self, n1, n2):
+  def __init__(self, ni_1, ni_2):
     # Path of nodes that this packet has traversed.
-    self.path = [n1]
+    self.path = [ni_1]
     self.dropped = False
     self.totalTime = 0
 
@@ -68,24 +72,56 @@ class NetworkSimulator:
     self.numNodes = numNodes
     self.numEdges = numEdges
     self.packets = []
+    avgEdgesPerNode = min(1, numEdges / numNodes)
 
-    self.nodes = [Node() for x in range(numNodes)]
+    self.nodes = []
+    self.edges = set()
 
-    # Randomly generate all possible edges then shuffle and truncate.
-    self.edges = [(x, y) for x in range(numNodes) for y in range(numNodes) if x != y]
-    shuffle(self.edges)
-    self.edges = self.edges[:numEdges]
+    # Randomly generate a connected graph with numNodes nodes
+    # and numEdges edges.
+    node = Node(0)
+    self.nodes.append(node)
 
-    for i in range(numEdges):
-      x, y = self.edges[i]
+    for i in range(1, numNodes):
+      node_1 = Node(i)
+      ni_1 = node_1.id
 
-      n1 = self.nodes[x]
-      n2 = self.nodes[y]
+      # Connect node to some n # of nodes already in self.nodes.
+      # Randomly generate number of nodes to connect.
+      numNodeEdges = avgEdgesPerNode
+      for j in range(min(len(self.nodes), numNodeEdges)):
+        ni_2 = rnd.randint(0, len(self.nodes) - 1)
+        edge = Edge(ni_1, ni_2)
+        
+        # Avoid double edges.
+        while (edge in self.edges): 
+          ni_2 = rnd.randint(0, len(self.nodes) - 1)
+          edge = Edge(ni_1, ni_2)
 
-      edge = Edge(n1, n2)
-      n1.addEdge(edge)
-      n2.addEdge(edge)
-      self.edges[i] = edge
+        node_1.addEdge(edge)
+        self.nodes[ni_2].addEdge(edge)
+
+      self.nodes.append(node_1)
+
+    # Check if we have enough edges. If not, then add edges 
+    # until we have enough edges.
+    # TODO: Hacky and redundant code. Can conslidate.
+    for i in range(numEdges - len(self.edges)):
+      ni_1 = rnd.randint(0, len(self.nodes) - 1)
+      ni_2 = rnd.randint(0, len(self.nodes) - 1)
+      while (ni_1 == ni_2): 
+        ni_2 = rnd.randint(0, len(self.nodes) - 1)
+
+      edge = Edge(ni_1, ni_2)
+      
+      # Avoid double edges.
+      while (edge in self.edges): 
+        ni_2 = rnd.randint(0, len(self.nodes) - 1)
+        edge = Edge(ni_1, ni_2)
+
+      node_1.addEdge(edge)
+      self.nodes[ni_2].addEdge(edge)
+      self.edges.add(edge)
 
   # TODO: Save network node and edge configurations into a file.
   def writeConfig(self):
@@ -95,27 +131,38 @@ class NetworkSimulator:
   def readConfig(self):
     return
 
-  # Route a single packet through a simulated route (an edge) 
-  # and return the destination note.
-  def routePacket(self, packet, edge):
+  # Route a single packet along an edge, updating its 
+  # total travel time and whether or not it's been dropped.
+  def traverseEdge(self, packet, edge):
     # Get most recently traveled node and find the other
     # node in the edge.
-    n1 = packet.getMostRecentNode()
-    n2 = edge.n2 if n1 == edge.n1 else n1
-    
-    packet.addToPath(n2)
+    ni_1 = packet.getMostRecentNode()
+    ni_2 = edge.ni_2 if ni_1 == edge.ni_1 else ni_1
+
+    packet.addToPath(ni_2)
     packet.totalTime += edge.getTravelTime()
     packet.isDropped = edge.isDropped()
     #TODO: Do something if packet is dropped.
 
-    return n2
+    return ni_2
+
+  # Route a single packet from packet.src to packet.dst.
+  def routePacket(self, packet):
+    # TODO: Flag to route packet according to a random policy vs. 
+    #       according to Q-values.
+    return 0
 
   # Generate n packets and simulate a route for all of them.
   def routePackets(self, n):
     # TODO: Generate a packet and have it routed thru
     # different nodes, adding to total travel time and
     # if dropped.
+    for k in range(n):
+      # TODO: Generate new packet.
+      # TODO: self.routePacket(packet).
+      break
+      
     return 0
 
-networkSimulator = NetworkSimulator(numNodes = 10, numEdges = 4)
+networkSimulator = NetworkSimulator(numNodes = 40, numEdges = 400)
 networkSimulator.routePackets(n = 1000)
